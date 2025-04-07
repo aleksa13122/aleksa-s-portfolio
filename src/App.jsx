a80450796d85de4685bcedda5684e4a7b2e9570f
aleksa13122 <akacar9@gmail.com>
2025-04-07 11:14:54 +0200

    Updated favicon and logo image

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 
-import gridImage from "./images/hexagonGray.png";
 import Navigation from "./components/navigation";
 import Info from "./components/Info";
 
@@ -11,12 +10,11 @@ function App() {
   return (
     <div>
       <div className="main-container">
-        <img src={gridImage} className="backgroundImg" alt="Grid background" />
         <Navigation />
         <Info />
       </div>
 
-      <p className="primarna">Aleksa's portfolio page!</p>
+      <p className="primarna">Aleksa's portfolio page</p>
     </div>
   );
 }
--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,12 +1,14 @@
+import { motion } from "framer-motion";
+
 import "./Navigation.css";
 
 export default function Navigation() {
   return (
     <div className="nav">
-      <button className="nav-div first">Home</button>
+      <button className="nav-div">Home</button>
       <button className="nav-div ">About</button>
       <button className="nav-div ">Projects</button>
-      <button className="nav-div last">Contact</button>
+      <button className="nav-div">Contact</button>
     </div>
   );
 }
