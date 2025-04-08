25353adcb16e536c8ac7ec109bf701a27fc85ee3
aleksa13122 <akacar9@gmail.com>
2025-04-08 22:26:40 +0200

    small changes

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -5,10 +5,10 @@ import "./Navigation.css";
 export default function Navigation() {
   return (
     <div className="nav">
-      <button className="nav-div">Home</button>
-      <button className="nav-div ">About</button>
+      <button className="nav-div">Homes</button>
+      <button className="nav-div ">Abouts</button>
       <button className="nav-div ">Projects</button>
-      <button className="nav-div">Contact</button>
+      <button className="nav-div">Contacts</button>
     </div>
   );
 }
