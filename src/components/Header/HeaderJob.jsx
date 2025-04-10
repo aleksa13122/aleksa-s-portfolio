9e1fba46c24fc5c7a8254c7745bc9d2272ea59f2
aleksa13122 <akacar9@gmail.com>
2025-04-10 22:41:15 +0200

    small changes

--- src/components/Header/HeaderJob.jsx
+++ src/components/Header/HeaderJob.jsx
@@ -1,11 +1,29 @@
 import "./Header.css";
 import HeaderLoc from "./HeaderLocation";
+import React from "react";
+import { useState } from "react";
+
+const words = [
+  "Hello",
+  "Здраво",
+  "Hola",
+  "你好",
+  "Bonjour",
+  "Привет",
+  "Γειά σου",
+  "Aloha",
+  "مرحبًا",
+];
 
 export default function HeaderJob() {
+  const [word, setWord] = useState(words[0]);
+
+  function changeWord() {}
+
   return (
     <div className="job-container">
       <div className="job-title">
-        <p className="greeting">Hi Im Aleksa</p>
+        <p className="greeting"> ,Im Aleksa</p>
         FRONTEND <br />
         <div className="second-row">
           <span className="sekundarna">DEVELOPER</span>
