07d7d365f6e094e2fe1991f9fcc52f5a15273656
aleksa13122 <akacar9@gmail.com>
2025-05-03 15:20:07 +0200

    media changes

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -67,8 +67,3 @@ const Header = forwardRef(({ goToProjects }, ref) => {
   );
 });
 export default Header;
-// It’s just a background div absolutely positioned behind the nav items.
-// I track the hovered tab using React useState,
-// and move the background using CSS transitions based on which tab is hovered.
-// The whole nav is set up with a CSS grid,
-// so each item takes up exactly a quarter of the space.
